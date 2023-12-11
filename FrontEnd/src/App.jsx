b420951500fafafa3ed4d26ce98d1cc00bb90134
HelloArtty <chaithawat.sakl@kmutt.ac.th>
2023-12-12 00:43:31 +0700

    pages and route

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 export default function App() {
   return (
-    <h1 className="text-10xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<Home />} />
+        <Route path='/about' element = {<About />} />
+        <Route path='/signin' element = {<SignIn />} />
+        <Route path='/signup' element = {<SignUp />} />
+        <Route path='/profile' element = {<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
