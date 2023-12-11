499d8e26b98ead0c45d8026a8320ca4cc512ce3e
HelloArtty <chaithawat.sakl@kmutt.ac.th>
2023-12-12 02:01:02 +0700

    connect to mongo

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,4 +1,17 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config();
+
+// Connect to DB
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB', err);
+    });
 
 const app = express();
 
