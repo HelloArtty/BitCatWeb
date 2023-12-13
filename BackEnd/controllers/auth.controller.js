420d9742c27a5607b757aa265e8d13147bb41dbb
HelloArtty <chaithawat.sakl@kmutt.ac.th>
2023-12-13 21:03:51 +0700

    complete sign in page

--- BackEnd/controllers/auth.controller.js
+++ BackEnd/controllers/auth.controller.js
@@ -8,22 +8,30 @@ export const signup = async(req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
+
     try {
         await newUser.save();
-        res.status(201).json('User created successfully');
-    }
-    catch (error) {
+        res.status(201).json({ success: true, message: 'User created successfully' });
+    } catch (error) {
+        if (error.code === 11000) {
+            let duplicateField;
+            if (error.message.includes('username')) {
+                duplicateField = 'Username';
+            } else if (error.message.includes('email')) {
+                duplicateField = 'Email';
+            }
+            return res.status(400).json({ success: false, message: `${duplicateField} is already taken.` });
+    }}
         next(error);
-    }
-};
+    };
 
 export const signin = async(req, res, next) => {
-    const { email, password } = req.body;
+    const { username, password } = req.body;
     try{
-        const validUser = await User.findOne({ email });
-        if (!validUser) return next(errorHandler(404, 'Use not found'));
+        const validUser = await User.findOne({ username });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
         const validPassword = bcrypt.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, 'Wrong credential!'));
+        if (!validPassword) return next(errorHandler(401, 'Enter the wrong password!'));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: pass, ...rest} = validUser._doc;
         res
